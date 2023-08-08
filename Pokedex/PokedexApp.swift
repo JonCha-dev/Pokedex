aedb75feaa178b5757aa9ada8b144a9a163c47ca
Jon Chang <jonasenchang@gmail.com>
2023-08-07 18:10:32 -0700

    Initial Commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Jon Chang on 8/7/23.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
